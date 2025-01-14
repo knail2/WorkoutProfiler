2be2a25d6a2d7857ad906b9d0261df4d0033607c
Omer Ansari <thecapablemanagerllc@gmail.com>
2025-01-13 22:39:30 -0600

    back to st.secrets

--- helpers/snowflake_connection.py
+++ helpers/snowflake_connection.py
@@ -11,10 +11,10 @@ def create_snowflake_connection():
 
     import snowflake.connector
     return snowflake.connector.connect(
-        user=os.getenv("SNOWFLAKE_USER"),
-        password=os.getenv("SNOWFLAKE_PASSWORD"),
-        account=os.getenv("SNOWFLAKE_ACCOUNT"),
-        warehouse=os.getenv("SNOWFLAKE_WAREHOUSE"),
-        database=os.getenv("SNOWFLAKE_DATABASE"),
-        schema=os.getenv("SNOWFLAKE_SCHEMA"),
+        user=st.secrets["snowflake"]["user"],
+        password=st.secrets["snowflake"]["password"],
+        account=st.secrets["snowflake"]["account"],
+        warehouse=st.secrets["snowflake"]["warehouse"],
+        database=st.secrets["snowflake"]["database"],
+        schema=st.secrets["snowflake"]["schema"]
     )
